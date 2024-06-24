506a535e437922e3a5423aacf7624b556e18f93e
Varalakshmi1389 <146834208+Varalakshmi1389@users.noreply.github.com>
2024-06-24 21:32:53 +0530

    hello.py

--- hello.py
+++ hello.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 st.write("My name is varalakshmi")
 st.write("Get visual based on movie name")
-x=st.text_input("Movie"."Star Wars")
+x=st.text_input("Movie","Star Wars")
 if st.button("Submit"):
   st.write(f"your favorite movie is '{x}'")
 data=pd.read_csv("movies.csv")
