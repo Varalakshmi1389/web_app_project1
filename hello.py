0132fb4628c44481dbd8d1b6d4c95f5cbf1b5977
Varalakshmi1389 <146834208+Varalakshmi1389@users.noreply.github.com>
2024-06-24 21:29:02 +0530

    hello.py

--- hello.py
+++ hello.py
@@ -2,3 +2,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 st.write("My name is varalakshmi")
+st.write("Get visual based on movie name")
+x=st.text_input("Movie"."star wars")
+if st.button("Submit"):
+  st.write(f"your favorite movie is '{x}'")
+  
