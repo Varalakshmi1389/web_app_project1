64138f9c6d7b6964dae401b11db627e2a3b8490b
Varalakshmi1389 <146834208+Varalakshmi1389@users.noreply.github.com>
2024-06-25 21:19:19 +0530

    previous_code.py

---
+++ previous_code.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+st.write("My name is varalakshmi")
+st.write("Get visual based on movie name")
+x=st.text_input("Movie","Star Wars")
+if st.button("Submit"):
+  st.write(f"your favorite movie is '{x}'")
+data=pd.read_csv("movies.csv")
+st.write(data)
+chart_data=pd.DataFrame(np.random.randn(20,3), columns=["a","b","c"])
+st.bar_chart(chart_data)
