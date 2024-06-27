1f5a4f3e0de960b884e804e4c273443445f2e866
Varalakshmi1389 <146834208+Varalakshmi1389@users.noreply.github.com>
2024-06-27 10:32:59 +0530

    hello.py

--- hello.py
+++ hello.py
@@ -16,21 +16,6 @@ st.set_page_config(
 
 df=pd.read_csv("inputfile.csv")
 st.write(df)
-import streamlit as st
-import pandas as pd
-
-# Set page configuration
-st.set_page_config(
-    page_title="Operations",
-    page_icon="🧊",
-    layout="wide",
-    initial_sidebar_state="expanded",
-    menu_items={
-        'Get Help': 'https://www.extremelycoolapp.com/help',
-        'Report a bug': "https://www.extremelycoolapp.com/bug",
-        'About': "# This is a header. This is an *extremely* cool app!"
-    }
-)
 
 st.title("Operation Data")
 
