57e663c7539462bd9af9993ff6e5ca0ebd140b65
Varalakshmi1389 <146834208+Varalakshmi1389@users.noreply.github.com>
2024-06-27 12:56:39 +0530

    input_data.py

--- input_data.py
+++ input_data.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-# Set page configuration
 st.set_page_config(
     page_title="Operations",
     page_icon="🧊",
@@ -15,20 +14,15 @@ st.set_page_config(
     }
 )
 
-# Load data
 df = pd.read_csv("inputfile.csv")
 st.write(df)
 
-# Convert CreationDate to datetime format and extract date
 df['Date'] = pd.to_datetime(df['CreationDate']).dt.date
-
-# Create a copy of the DataFrame
 df1 = df.copy()
 
 # Group by UserId, Date, and Operation to count occurrences
 summary_df = df1.groupby(['UserId', 'Date', 'Operation']).size().reset_index(name='Count of Operations')
 
-# Aggregate the count of operations by UserId
 final_summary_df = summary_df.groupby('UserId').agg({
     'Date': 'first',
     'Operation': 'first',
