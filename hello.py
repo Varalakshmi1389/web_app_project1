eab7f4696d3c82f3aeb42fb3e5897d8e2ccb5481
Varalakshmi1389 <146834208+Varalakshmi1389@users.noreply.github.com>
2024-06-27 11:39:47 +0530

    hello.py

--- hello.py
+++ hello.py
@@ -20,7 +20,7 @@ st.write(df)
 df1=df.copy()
 
 df1['Date'] = pd.to_datetime(df1['CreationDate']).dt.date
-
+#Creating table with count of operations based userid,date and opeartion
 summary_df = df1.groupby(['UserId', 'Date', 'Operation']).size().reset_index(name='Count of Operations')
 
 final_summary_df = summary_df.groupby('UserId').agg({
@@ -31,6 +31,15 @@ final_summary_df = summary_df.groupby('UserId').agg({
 st.subheader("Operation Count by UserId, Date, and Operation")
 st.table(final_summary_df[['UserId', 'Date', 'Operation', 'Count of Operations']])
 
+record_type_summary = df1.groupby('Operation')['RecordType'].sum().reset_index()
+
+# Plotting pie chart for sum of RecordType by Operation
+fig, ax = plt.subplots()
+ax.pie(record_type_summary['RecordType'], labels=record_type_summary['Operation'], autopct='%1.1f%%', startangle=90)
+ax.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+
+st.subheader("Pie Chart: Sum of RecordType by Operation")
+st.pyplot(fig)
 
 
 
