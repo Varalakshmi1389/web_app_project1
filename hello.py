36c534b17d53517a3c5b821614fd0557efc56afc
Varalakshmi1389 <146834208+Varalakshmi1389@users.noreply.github.com>
2024-06-27 10:36:20 +0530

    hello.py

--- hello.py
+++ hello.py
@@ -18,23 +18,10 @@ df=pd.read_csv("inputfile.csv")
 st.write(df)
 
 st.title("Operation Data")
-
-# File uploader
-uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
-
-if uploaded_file is not None:
-    # Read the CSV file
-    df = pd.read_csv(uploaded_file)
-
-    # Display the DataFrame in Streamlit
-    st.subheader("Data Table")
-    st.table(df)
-
-    # Generate summary table
-    summary_df = df.groupby(['Date', 'Operation', 'Email ID']).size().reset_index(name='Count of Operation')
+df1=df.copy()
+    summary_df = df1.groupby(['Date', 'Operation', 'Email ID']).size().reset_index(name='Count of Operation')
 
     st.subheader("Summary Table")
     st.table(summary_df)
-else:
-    st.write("Please upload a CSV file to see the data.")
+
 
