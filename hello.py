e1c87faed755a65c88b2eefb7be4bd451a8d312e
Varalakshmi1389 <146834208+Varalakshmi1389@users.noreply.github.com>
2024-07-04 22:32:25 +0530

    hello.py

--- hello.py
+++ hello.py
@@ -8,3 +8,17 @@ penguins = pd.read_csv(url)
 
 # Display the first few rows of the dataset
 print(penguins.head())
+
+# Page configuration should be set before any Streamlit elements are created
+st.set_page_config(
+    page_title="png",
+    page_icon="🧊",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        'Get Help': 'https://www.extremelycoolapp.com/help',
+        'Report a bug': "https://www.extremelycoolapp.com/bug",
+        'About': "# This is a header. This is an *extremely* cool app!"
+    }
+)
+
